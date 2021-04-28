b002268eec68dfdd2cdc202ea3c1c3c02770cfd6
ethan <ethan@example.com>
2021-04-28 21:50:03 +0800

    4月的第二次提交

--- src/main.js
+++ src/main.js
@@ -5,11 +5,24 @@ import store from './store'
 
 import toast from 'components/common/toast'
 
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
 Vue.config.productionTip = false
 
+// 添加事件总线对象
 Vue.prototype.$bus = new Vue()
 
+//安装toast插件
 Vue.use(toast)
+
+//解决移动端300ms延迟：fastclick
+FastClick.attach(document.body)
+
+//使用图片懒加载的插件
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
